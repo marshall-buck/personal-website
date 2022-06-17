31941de6e56e8dd6c8548dba3589cdb84b491dbc
Marshall Buck <dev@marshallbuck.com>
2022-06-17 12:12:18 -0500

    added fredoka font to css file
    added reset.css, landing page is static
    
    navbar in place, no functionality
    
    added main.js
    
    animate from landing to work, and play sections work, need to get back to landing
    
    before rebase
    
    added home icon
    
    contact link opens and closes blank form
    
     link navigation works, all sections animate properly
    
    added images, need to style
    
    play animation works, but need to re-adjust play mask leaving stage

---
+++ main.js
@@ -0,0 +1,65 @@
+const workLink = document.querySelector('#work-link');
+const workSection = document.querySelector('#work');
+const navText = document.querySelectorAll('nav, nav > a');
+const formContainer = document.querySelector("#contact");
+const closeForm = document.querySelector("#contact > div");
+const playLink = document.querySelector('#play-link');
+const playSection = document.querySelector('#play');
+const nav = document.querySelector('nav');
+const workMask = document.querySelector('#work > .mask');
+const playMask = document.querySelector('#play > .mask');
+const carousel = document.querySelector('#carousel');
+
+const active = { currentPage: 'contact-link' };
+closeForm.addEventListener('click', () => { formContainer.classList.toggle("invisible"); });
+
+nav.addEventListener('click', (e) => {
+  const id = e.target.id;
+  active.currentPage = id;
+
+
+  if (active.currentPage === "home-link") {
+    setNavText('var(--dark-purple)');
+    workSection.setAttribute('class', "container half-page");
+    playSection.setAttribute('class', 'container half-page');
+    formContainer.setAttribute('class', 'container invisible');
+    carousel.setAttribute('class', 'container zero-page');
+    playMask.setAttribute('class', 'mask');
+  }
+  if (active.currentPage === "contact-link") {
+    formContainer.classList.toggle("invisible");
+  }
+  if (active.currentPage === "work-link") {
+    if (workSection.classList.contains('full-page')) return;
+    setNavText('var(--dark-purple)');
+    playSection.setAttribute('class', "container zero-page");
+    workSection.setAttribute('class', 'container full-page');
+    carousel.setAttribute('class', 'container zero-page');
+
+
+
+  }
+  if (active.currentPage === "play-link") {
+
+    if (playSection.classList.contains('full-page')) return;
+    setNavText('var(--dark-yellow)');
+    workSection.setAttribute('class', "container zero-page");
+    playSection.setAttribute('class', 'container full-page');
+    playMask.setAttribute('class', 'play-invisible');
+    carousel.setAttribute('class', 'container full-page');
+
+
+  }
+
+
+
+});
+/*TODO: Create func to loop through nav links and set active class*/
+function setNavText(color) {
+  for (let e of navText) {
+    e.style.color = color;
+
+
+  }
+}
+
